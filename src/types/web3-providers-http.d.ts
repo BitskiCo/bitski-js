9e1b407eec955c19cfaddc86ad041961d924a6cb
Julian Tescher <jatescher@gmail.com>
2018-03-27 16:17:55 -0700

    Add publish scripts

--- src/types/web3-providers-http.d.ts
+++ src/types/web3-providers-http.d.ts
@@ -1,31 +1,32 @@
 declare module 'web3-providers-http' {
     export interface JsonRPCRequest {
-        jsonrpc: string
-        method: string
-        params: any[]
-        id: number
+        jsonrpc: string;
+        method: string;
+        params: any[];
+        id: number;
     }
     export interface JsonRPCResponse {
-        jsonrpc: string
-        id: number
-        result?: any
-        error?: string
+        jsonrpc: string;
+        id: number;
+        result?: any;
+        error?: string;
     }
 
     export type JsonRPCCallback = (e: Error, val: JsonRPCResponse) => void;
 
     export default class HttpProvider {
-        responseCallbacks: undefined
-        notificationCallbacks: undefined
-        connection: undefined
-        addDefaultEvents: undefined
-        on(type: string, callback: () => any): undefined
-        removeListener(type: string, callback: () => any): undefined
-        removeAllListeners(type: string): undefined
-        reset(): undefined
+        public responseCallbacks: undefined;
+        public notificationCallbacks: undefined;
+        public connection: undefined;
+        public addDefaultEvents: undefined;
 
         constructor(host: string, timeout: number, headers: any);
 
-        send(payload: JsonRPCRequest, callback: JsonRPCCallback): void
+        public on(type: string, callback: () => any): undefined;
+        public removeListener(type: string, callback: () => any): undefined;
+        public removeAllListeners(type: string): undefined;
+        public reset(): undefined;
+
+        public send(payload: JsonRPCRequest, callback: JsonRPCCallback): void;
     }
 }
