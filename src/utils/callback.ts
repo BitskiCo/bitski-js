d086425ec526a2285aa8c2a309ba22d20b63fa90
Josh Pyles <joshpyles@gmail.com>
2018-11-13 15:06:06 -0800

    Add callback script to simplify popup and silent login

---
+++ src/utils/callback.ts
@@ -0,0 +1,70 @@
+/**
+ * Automatically handles finalizing the oauth sign in process with the Bitski SDK
+ */
+function processCallback() {
+  if (window.opener) {
+    notifyOpener(window.location.href);
+  } else if (window.parent && window.parent !== window) {
+    notifyParent(window.location.href);
+  } else {
+    throw new Error('Callback opened on invalid window');
+  }
+}
+
+/**
+ * Extracts query params from the hash of the url
+ * @param url the url to parse
+ */
+function parseUrlParams(url: string): any {
+  if (url.includes('#')) { throw new Error('No params found in result'); }
+
+  let params = url.split('#').pop();
+
+  if (!params) { throw new Error('No params found in result'); }
+
+  return params.split("&").reduce((prev, item) => {
+    let [key, value] = item.split('=');
+    if (key && value) {
+      prev[decodeURIComponent(key)] = decodeURIComponent(value);
+    }
+    return prev;
+  }, {});
+}
+
+/**
+ * Notifies the parent window when in an iframe
+ * @param url the url that contains the query params
+ */
+function notifyParent(url: string): void {
+  if (window.parent && window !== window.parent) {
+    window.parent.postMessage(url, location.protocol + "//" + location.host);
+  } else {
+    throw new Error('Could not notify parent');
+  }
+}
+
+/**
+ * Notifies the opener when in a popup
+ * @param url the url that contains the query params
+ */
+function notifyOpener(url: string): void {
+  if (window.opener) {
+    if (url) {
+      // parse url to get state
+      const data = parseUrlParams(url);
+      if (data.state) {
+        const name = `popupCallback_${data.state}`;
+        const callback = window.opener[name];
+        if (callback) {
+          callback(url);
+        } else {
+          throw new Error('No callback found on opener');
+        }
+      } else {
+        throw new Error('No state found in response');
+      }
+    }
+  } else {
+    throw new Error('No window.opener');
+  }
+}
