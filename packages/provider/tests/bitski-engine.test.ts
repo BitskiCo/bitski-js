edd889a8b6f7a524d8eb6d9374a8f24463f12b8f
Josh Pyles <joshpyles@gmail.com>
2019-01-14 13:50:21 -0800

    Fix issue with using our provider with Truffle

--- packages/provider/tests/bitski-engine.test.ts
+++ packages/provider/tests/bitski-engine.test.ts
@@ -3,3 +3,12 @@ import { BitskiEngine } from "../src";
 test('it exists', () => {
   const engine = new BitskiEngine({});
 });
+
+test('it forwards sends when callback is present', () => {
+  const engine = new BitskiEngine({});
+  engine.start();
+  const spy = jest.spyOn(engine, 'sendAsync');
+  // @ts-ignore
+  engine.send({}, jest.fn());
+  expect(spy).toBeCalled();
+});
