52b767952c70e942d75ebe74b75531b874a3fcd2
Thinh Vu <thinh@bitski.com>
2023-05-02 14:07:57 -0400

    Update examples to include request accounts to work with extension

--- examples/ethers/src/App.jsx
+++ examples/ethers/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
-import { Bitski, AuthenticationStatus } from "bitski";
-import { ethers } from "ethers";
+import { useState, useEffect } from 'react';
+import { Bitski, AuthenticationStatus } from 'bitski';
+import { ethers } from 'ethers';
 
 const bitski = new Bitski(
-  "1812bcfa-44ab-48e3-87b2-b06de6c8e89d",
-  "https://e2tc5f-5173.csb.app/callback.html"
+  '1812bcfa-44ab-48e3-87b2-b06de6c8e89d',
+  'http://localhost:5173/callback.html',
 );
 
 function App() {
   const [currentAccount, setAccount] = useState(null);
   const [provider, setProvider] = useState(null);
   const [hash, setHash] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     getProvider();
@@ -23,7 +24,25 @@ function App() {
     setProvider(web3);
   };
 
+  const getAccounts = async () => {
+    const accounts = await provider.send('eth_accounts');
+
+    if (accounts.length) {
+      return accounts;
+    }
+
+    const addressAccounts = await provider.send('eth_requestAccounts');
+
+    if (addressAccounts.length) {
+      return addressAccounts;
+    }
+
+    setError('Could not find valid accounts');
+  };
+
   const connect = async () => {
+    setError(null);
+
     if (!provider) {
       getProvider();
     }
@@ -34,7 +53,7 @@ function App() {
       await bitski.signIn();
     }
 
-    const accounts = await provider.send("eth_accounts");
+    const accounts = getAccounts();
 
     if (accounts && accounts[0]) {
       setAccount(accounts[0]);
@@ -45,12 +64,13 @@ function App() {
     await bitski.signOut();
     setAccount(null);
     setHash(null);
+    setError(null);
   };
 
   const signMessage = async () => {
-    const transactionHash = await provider.send("eth_sign", [
+    const transactionHash = await provider.send('eth_sign', [
       currentAccount,
-      "This is a test message you are signing.",
+      'This is a test message you are signing.',
     ]);
 
     setHash(transactionHash);
@@ -59,12 +79,34 @@ function App() {
   return (
     <main className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-4xl">Bitski + ethers</h1>
+
+      {error && (
+        <div class="alert alert-error shadow-lg max-w-xs my-4">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>{error} </span>
+          </div>
+        </div>
+      )}
+
       <section className="flex flex-col items-center justify-center">
         <button
           className="my-4 inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
           onClick={() => (currentAccount ? disconnect() : connect())}
         >
-          {currentAccount ? "Disconnect" : "Sign In With Bitski"}
+          {currentAccount ? 'Disconnect' : 'Sign In With Bitski'}
         </button>
 
         {currentAccount ? (
@@ -73,7 +115,7 @@ function App() {
             <p className="mt-2 font-bold">{currentAccount}</p>
           </div>
         ) : (
-          "Not logged in."
+          'Not logged in.'
         )}
 
         {currentAccount && !hash ? (
