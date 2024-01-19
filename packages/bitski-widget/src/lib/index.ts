5d84fd356beb80ad135ef928a4fde3d89d326da2
Thinh Vu <thinh@bitski.com>
2024-01-19 17:22:58 -0500

    Add LoginMethod export

--- packages/bitski-widget/src/lib/index.ts
+++ packages/bitski-widget/src/lib/index.ts
@@ -1,5 +1,6 @@
 import './index.css';
 export { BitskiProvider, BitskiWidget } from './components/BitskiWidget';
 export type { LoginMethods } from './components/BitskiWidget/types';
+export { LoginMethod } from './components/BitskiWidget/constants';
 export { bitski, phantom } from './connectors';
 export { createBitskiConfig } from './utils';
