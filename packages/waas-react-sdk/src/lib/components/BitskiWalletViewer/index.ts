69eee40085b30787d1049f41cf33dd30c7b1c6b6
Thinh Vu <4204797+chronicIntrovert@users.noreply.github.com>
2024-01-31 23:11:35 -0500

    Add extra exports to index (#467)

--- packages/waas-react-sdk/src/lib/components/BitskiWalletViewer/index.ts
+++ packages/waas-react-sdk/src/lib/components/BitskiWalletViewer/index.ts
@@ -1,1 +1,3 @@
 export { default as BitskiWalletViewer } from './BitskiWalletViewer';
+export { BitskiWalletProvider, WalletViewerContext } from './BitskiWalletProvider';
+export { ChainIcon } from './ChainIcon';
--- packages/waas-react-sdk/src/lib/index.ts
+++ packages/waas-react-sdk/src/lib/index.ts
@@ -1,6 +1,13 @@
 import './index.css';
 export { BitskiProvider, BitskiConnect, BitskiWidget } from './components/BitskiWidget';
+export {
+  BitskiWalletProvider,
+  BitskiWalletViewer,
+  WalletViewerContext,
+  ChainIcon,
+} from './components/BitskiWalletViewer';
 export type { LoginMethods } from './components/BitskiWidget/types';
+export { Tab } from './components/BitskiWalletViewer/constants';
 export { LoginMethod } from './components/BitskiWidget/constants';
 export { bitski, phantom } from './connectors';
 export { createBitskiConfig } from './utils';
